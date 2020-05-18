22283772901211033711c9d44fb2ad0f224dae2d
chenlei <1217221092@qq.com>
2020-05-18 14:22:51 +0800

    config

--- 08.Spring-Cloud-Config/Config-Client/src/main/java/com/example/demo/ConfigClientApplication.java
+++ 08.Spring-Cloud-Config/Config-Client/src/main/java/com/example/demo/ConfigClientApplication.java
@@ -9,12 +9,4 @@ public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
     }
-
-    /** 解决 Could not resolve placeholder xxx问题 */
-//    @Bean
-//    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
-//        PropertySourcesPlaceholderConfigurer c = new PropertySourcesPlaceholderConfigurer();
-//        c.setIgnoreUnresolvablePlaceholders(true);
-//        return c;
-//    }
 }
